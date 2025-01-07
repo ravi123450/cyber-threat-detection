8fa2c3accbfb908208e663a05ccf56eeafd103c7
ravi123450 <141273837+ravi123450@users.noreply.github.com>
2025-01-07 13:11:33 +0530

    Update settings.py

--- cyberproj/settings.py
+++ cyberproj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-743ddji6ngod1i&162mmeyyus5m*mouu!)*(7y-=+0e$u14!$m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cyber-threat-detection.onrender.com']
 
